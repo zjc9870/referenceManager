3c70ced931a0dc5cd53094f491e278a5826c9d1f
Jun Liu <1135473244@qq.com>
2018-05-09 22:46:47 +0800

    lj-5.9

--- src/main/java/syuu/service/MomentService.java
+++ src/main/java/syuu/service/MomentService.java
@@ -2,21 +2,15 @@ package syuu.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import syuu.dataObject.Moment;
-import syuu.dataObject.Reference;
-import syuu.dataObject.User;
-import syuu.repository.MomentRepository;
-import syuu.repository.ReferenceRepository;
-import syuu.repository.UserRepository;
-import syuu.service.VO.CommentVo;
-import syuu.service.VO.MomentVo;
-import syuu.service.VO.ReferenceVo;
-import syuu.service.VO.UserVo;
-
-import java.text.SimpleDateFormat;
+import syuu.dataObject.*;
+import syuu.repository.*;
+import syuu.service.VO.*;
+import java.util.Comparator;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Collections;
 import java.util.List;
+
+import java.util.*;
 
 @Service
 public class MomentService {
@@ -27,43 +21,148 @@ public class MomentService {
     UserRepository userRepository;
     @Autowired
     ReferenceRepository referenceRepository;
-    public List<MomentVo> getMomentByUser(UserVo user) {
+    @Autowired
+    CommentRepository commentRepository;
+    @Autowired
+    FriendRepository friendRepository;
+    @Autowired
+    LikeRepository likeRepository;
+    @Autowired
+    LikeService likeService;
+    @Autowired
+    YdCommentRepository ydCommentRepository;
+    @Autowired
+    UserService userService;
+
+    public List<Moment> getMoment(UserVo user)
+    {
         List<Moment> momentList = momentRepository.findMomentByUser(userRepository.findOne(user.getId()));
+        if(momentList!=null)
+            return  momentList;
+        else
+            return null;
+
+    }
+
+    public List<MomentVo> getMomentByUser(List<Moment> momentList,UserVo user,UserVo user1) {
+        //System.out.println("调用getMomentByUser函数");
+        //List<Moment> momentList = momentRepository.findMomentByUser(userRepository.findOne(user.getId()));
         List<MomentVo> momentVoList = new ArrayList<MomentVo>();
-        if(momentList!=null){
-            for(Moment moment:momentList){
+
+
+
+        if(momentList!=null) {
+            boolean addmoment;
+            for (Moment moment : momentList) {
+                addmoment = true;//用于屏蔽列表控制朋友圈的添加。
+
                 int id = moment.getId();
                 String content = moment.getContent();
                 Date time = moment.getTime();
+
+
                 List<UserVo> blockList = new ArrayList<UserVo>();
-                if(moment.getBlockList()!=null){
-                    for(User blockUser:moment.getBlockList()){
+                if (moment.getBlockList() != null) {
+                    for (User blockUser : moment.getBlockList()) {
+                        if (blockUser.getId() == user1.getId())
+                            addmoment = false;
                         blockList.add(new UserVo(blockUser));
                     }
                 }
+
+
                 List<ReferenceVo> referenceVoList = new ArrayList<ReferenceVo>();
-                if(moment.getReference()!=null){
-                    for(Reference reference:moment.getReference()){
+                if (moment.getReference() != null) {
+                    for (Reference reference : moment.getReference()) {
                         referenceVoList.add(new ReferenceVo(reference));
                     }
                 }
+
+
+                List<LikeVo> likeVoList = new ArrayList<LikeVo>();
+                List<Like> likeList = likeRepository.findLikeByMoment(moment);
+                if (likeList != null) {
+                    for (Like like : likeList) {
+                        likeVoList.add(new LikeVo(like));
+                    }
+                }
+
+
                 List<CommentVo> commentVoList = new ArrayList<CommentVo>();
-                UserVo userVo = new UserVo(userRepository.findOne(user.getId()));
-                MomentVo momentVo = new MomentVo(id,content,time,user,blockList,referenceVoList,commentVoList);
-                momentVoList.add(momentVo);
+                List<Comment> commentList = commentRepository.findCommentByMoment(moment);
+                if (commentList != null) {
+                    boolean addcomment = false;
+                    //只有评论的双方都在好友列表中，该用户才能看见这条评论
+                    for (Comment comment : commentList) {
+                        addcomment = false;
+
+                        if (user1.getFriendList() != null) {
+                            for (UserVo friendVo : user1.getFriendList()) {
+
+                                if (friendVo.getId() == comment.getFromuser().getId() || user1.getId() == comment.getFromuser().getId()) {
+                                    for (UserVo friendVo1 : user1.getFriendList()) {
+                                        if (friendVo1.getId() == comment.getTouser().getId() || user1.getId() == comment.getTouser().getId()) {
+                                            addcomment = true;
+                                            continue;
+                                        }
+                                    }
+                                }
+                            }
+                        } else
+                            System.out.println("好友列表为NULL！");
+
+                        if (addcomment == true) {
+                            commentVoList.add(new CommentVo(comment));
+
+                        }
+                    }
+                }
+
+
+                List<CommentVo> ydCommentVoList = new ArrayList<CommentVo>();
+                List<YdComment> ydCommentList = ydCommentRepository.findYdCommentByTouser(userRepository.findOne(user1.getId()));
+
+                UserVo userVo = new UserVo(userRepository.findOne(moment.getUser().getId()));
+                MomentVo momentVo = new MomentVo(id, content, time, userVo, blockList, referenceVoList, commentVoList, likeVoList.size(), ydCommentVoList,user);
+                if (addmoment == true) {
+                    momentVoList.add(momentVo);
+                }
             }
+
+
         }
         return momentVoList;
     }
+
+
     //获取该用户应该看到的全部朋友圈
     public List<MomentVo> getMoments(UserVo user){
         List<MomentVo> momentVoList = new ArrayList<MomentVo>();
+        List<Moment> momentList=new ArrayList<Moment>();
+
         if(user.getFriendList()!=null){
             for(UserVo friendVo:user.getFriendList()){
-                momentVoList.addAll(getMomentByUser(friendVo));
+                if(getMoment(friendVo)!=null)
+                    momentList.addAll(getMoment(friendVo));
             }
         }
-        momentVoList.addAll(getMomentByUser(user));
+        momentList.addAll(getMoment(user));
+
+        sortClass sort=new sortClass();
+        Collections.sort(momentList,sort);
+        System.out.println("LIST NUMBER:"+momentList.size());
+
+
+        for(int i=0;i<momentList.size();i++){
+            Moment moment=momentList.get(i);
+            System.out.println("姓名:"+moment.getUser().getUsername()+",生日:"+moment.getTime());
+        }
+
+
+
+        momentVoList.addAll(getMomentByUser(momentList,user,user));
+
+
         return momentVoList;
     }
 
@@ -84,5 +183,30 @@ public class MomentService {
         moment.setReference(referenceList);
         moment.setUser(userRepository.findOne(momentVo.getUser().getId()));
         momentRepository.save(moment);
+    }
+
+
+    public int getydCommentNum(UserVo user1)
+    {
+        List<YdComment> ydCommentList=ydCommentRepository.findYdCommentByTouser(userRepository.findOne(user1.getId()));
+        int size=ydCommentList.size();
+        for(YdComment ydComment:ydCommentList)
+        {
+            ydCommentRepository.delete(ydComment.getId());
+        }
+
+        return size;
+    }
+}
+class sortClass implements Comparator
+{
+    public int compare(Object o1,Object o2)
+    {
+        Moment m1=(Moment)o1;
+        Moment m2=(Moment)o2;
+        if(m1.getTime().before(m2.getTime()))
+            return 1;
+        else
+            return -1;
     }
 }
